055e39a079348a867af483d688b84073fdeddb4d
NicoloRicciardo <richmnico@gmail.com>
2021-05-04 22:19:29 +0200

    Continuazione creazione login e registrazione. Ristrutturato il sistema di crazione activity: inizio sostituzione activity con fragment.

--- app/src/main/java/it/unimib/letsdrink/ui/profile/AccessProfileActivity.java
+++ app/src/main/java/it/unimib/letsdrink/ui/profile/AccessProfileActivity.java
@@ -1,11 +1,13 @@
 package it.unimib.letsdrink.ui.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import it.unimib.letsdrink.R;
 
@@ -13,13 +15,34 @@ public class AccessProfileActivity extends AppCompatActivity {
 
     private Button btn_registrazione;
     private com.google.android.material.button.MaterialButton btn_accedi;
+    UserAccessFragment accessFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access_profile);
 
-        setBtn_registrazione();
+        setAccessFragment();
+        onCloseActivity();
+        //setBtn_registrazione();
+    }
+
+    private void setAccessFragment() {
+        accessFragment = new UserAccessFragment();
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.access_frame, accessFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void onCloseActivity() {
+        ImageButton btn_close = findViewById(R.id.button_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private void setBtn_registrazione() {
@@ -32,8 +55,6 @@ public class AccessProfileActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
-
-
     }
 
     private void setBtn_accedi() {
